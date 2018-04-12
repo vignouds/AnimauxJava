37b30e9e29ecfbed5d7955e62dcfcfd9eafabe25
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-04-12 14:03:18 +0200

    ajout de toString dans regime

--- src/animal/regime/Regime.java
+++ src/animal/regime/Regime.java
@@ -1,8 +1,14 @@
 package animal.regime;
 
 public abstract class Regime {
+	private String regime;
+	
 	public Regime() {
-		
+		this.regime=" ";
+	}
+	
+	public String toString() {
+		return "("+regime+")";
 	}
 
 }
