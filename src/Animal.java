fb5562f598ec2c903c8b04381f118f314ae6a5ca
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-04-12 10:56:36 +0200

    creation classe Animal avec constructeur

---
+++ src/Animal.java
@@ -0,0 +1,15 @@
+
+public abstract class Animal {
+	private int age, poids, taille;
+	private String nom;
+	
+	public Animal() {
+		this.age=0;
+		this.poids=0;
+		this.taille=0;
+		this.nom=" ";
+		
+		// manger(Aliment aliment)
+		//deplacer()
+	}
+}
